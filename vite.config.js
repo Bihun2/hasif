848bf295ed3463d8695af75bb383216f5eb5c294
Bihun2 <shadowkjm@gmail.com>
2025-06-02 07:01:07 +0800

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',  // Set this to your repo name if not served from domain root
+  base: '/hasif/',  // Set this to your repo name if not served from domain root
   build: {
     outDir: 'dist',
     emptyOutDir: true,
